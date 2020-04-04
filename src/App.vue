282498aa42a28c3421f0f75021601996f6bda5ff
Asim <asimsyed0322@gmail.com>
2020-04-04 20:03:47 +0500

    Starter Project

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <Todos />
+  </div>
+</template>
+
+<script>
+
+import Todos from './components/Todos';
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data(){
+    return{
+      todos : [
+        {
+          id:1,
+          title:"Todo One",
+          completed:false
+        },
+        {
+          id:2,
+          title:"Todo Two",
+          completed:true
+        },
+        {
+          id:3,
+          title:"Todo Three",
+          completed:false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
